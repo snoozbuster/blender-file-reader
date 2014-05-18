c6d73e25798eaed38530161db248bacb34311786
Alex Van Liew <snoozbuster@yahoo.com>
2014-05-18 00:32:35 -0700

    Added skeleton XNA project, changed library to .NET4

---
+++ XNADriver/XNADriver/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace XNADriver
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (BaseGame game = new BaseGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
